c233f32fd0322d2bc8f9b4e50cb74fcaf74cca47
prateek951 <prince.cenation@gmail.com>
2018-09-17 01:29:01 +0530

    create a profile postreq

--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,90 @@
 // Full Documentation - https://www.turbo360.co/docs
-const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
-const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const turbo = require("turbo360")({ site_id: process.env.TURBO_APP_ID });
+const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
 const router = vertex.router();
-const HTTP_STATUS = require('http-status-codes');
-const Profile = require('../models/Profile');
+const HTTP_STATUS = require("http-status-codes");
+const Profile = require("../models/Profile");
 
 //@desc('GET','/profiles','retrieve the list of all the profiles')
 
-router.get('/profiles',(req, res) => {
+router.get("/profiles", (req, res) => {
+  const query = req.query;
 
-	const query = req.query;
-
-	Profile.find(query).then(profiles => res.status(HTTP_STATUS.OK).json({
-		confirmation: 'success',
-		data: profiles
-	})).catch(err => res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-		confirmation: 'fail',
-		message: err.message
-	}));
+  Profile.find(query)
+    .then(profiles =>
+      res.status(HTTP_STATUS.OK).json({
+        confirmation: "success",
+        data: profiles
+      })
+    )
+    .catch(err =>
+      res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+        confirmation: "fail",
+        message: err.message
+      })
+    );
 });
 
 //@desc('GET','/profiles/ds','retrieve the list of those profiles where team is datascientists')
 
-router.get('/profiles/ds',(req, res) => {
-	Profile.find({team: 'datascientists'})
-	.then(profiles => {
-		res.status(HTTP_STATUS.OK).json({
-			confirmation: 'success',
-			data: profiles
-		});
-	})
-	.catch(err => {
-		res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-			confirmation: 'fail',
-			message: err.message
-		});
-	});
+router.get("/profiles/ds", (req, res) => {
+  Profile.find({ team: "datascientists" })
+    .then(profiles => {
+      res.status(HTTP_STATUS.OK).json({
+        confirmation: "success",
+        data: profiles
+      });
+    })
+    .catch(err => {
+      res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+        confirmation: "fail",
+        message: err.message
+      });
+    });
 });
-
 
 //@desc('GET','/profiles/:id','Retrieve a specific profile')
 
-router.get('/profiles/:id',async (req, res) => {
-	try {
-		const { id } = req.params;
-		const profile = await Profile.findById(id);
-		if (profile) res.status(HTTP_STATUS.OK).json({confirmation: 'success', data: profile }); 
-	} catch (error) {
-		res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-			confirmation: 'fail',
-			message: error.message
-		});
-	}
-	
-})
+router.get("/profiles/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const profile = await Profile.findById(id);
+    if (profile)
+      res
+        .status(HTTP_STATUS.OK)
+        .json({ confirmation: "success", data: profile });
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      confirmation: "fail",
+      message: `Profile ${id} not found`
+    });
+  }
+});
 
+//@desc('POST','/profile','Create a profile')
 
-module.exports = router
+router.post("/profile", (req, res) => {
+  // const { firstname, lastname, age, position, team } = req.body;
+  // res.status(HTTP_STATUS.OK).json({
+  // 	confirmation: 'success',
+  // 	data: req.body
+  // })
+  Profile.create(req.body)
+    .then(profile => {
+      if (profile)
+        res
+          .status(HTTP_STATUS.OK)
+          .json({
+            confirmation: "success",
+            data: profile,
+            message: "Profile got created"
+          });
+    })
+    .catch(err =>
+      res
+        .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+        .json({ confirmation: "fail", message: err.message })
+    );
+});
+
+module.exports = router;
