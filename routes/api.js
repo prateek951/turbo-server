02035bc1a242c8967cb3a8c9b597a22279c5bc34
prateek951 <prince.cenation@gmail.com>
2018-09-16 23:47:37 +0530

    serving json on get req.

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,16 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const Profile = require('../models/Profile');
 
-/*  This is a sample API route. */
+//@desc('GET','/','retrieve the profile')
 
-router.get('/:resource', (req, res) => {
+router.get('/profile',(req, res) => {
 	res.json({
 		confirmation: 'success',
-		resource: req.params.resource,
-		query: req.query // from the url query string
-	})
-})
-
-router.get('/:resource/:id', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		id: req.params.id,
-		query: req.query // from the url query string
-	})
-})
-
+		date : 'This is the profile endpoint'
+	});
+});
 
 
 module.exports = router
